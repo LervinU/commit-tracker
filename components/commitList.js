91567aa58d4da7fa0d7c5bbaa5482cbd5615c061
Lervin Urena <lurena@contractor.appsbees.com>
2022-12-27 18:45:00 -0400

    Feat: Create CommitList functional component

--- components/commitList.js
+++ components/commitList.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import useSWR from 'swr'
+import CommitCard from './commitCard';
+// import constants from '../utils/constants'
 
-export default function commitList() {
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+export default function CommitList() { 
+    const { data, error, isLoading } = useSWR(`http://localhost:4000/commits?page=4`, fetcher)
+
+    if (error) return <div>failed to load</div>
+    if (isLoading) return <div>loading...</div>
+
     return (
         <div>
+            {data.map(commit => {
+                return (
+                    <div>
+                        <CommitCard key={commit.sha} avatar={commit.avatar_url} authorName={commit.author.name} message={commit.message}/>
+                    </div>
+                )
+            })}
             
         </div>
     )
