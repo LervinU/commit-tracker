fa559c8cde3a54e11b70cd61f9b40e72ae2be26d
Lervin Urena <lurena@contractor.appsbees.com>
2022-12-27 18:19:59 -0400

    Feat: Create and customize CommitCard component

--- components/commitCard.js
+++ components/commitCard.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default function commitCard() {
+export default function CommitCard(props) {
     return (
-        <div>
-            
+
+        <div class="p-6 mb-4 max-w-2xl mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+            <div class="shrink-0">
+                <img class="h-12 w-12" src={props.avatar} alt="Profile picture"/>
+            </div>
+            <div>
+                <div class="text-xl font-medium text-black">{props.authorName}</div>
+                <p class="text-slate-500">{props.message}</p>
+            </div>
         </div>
+
     )
 }
