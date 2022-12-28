241f89e8edc2ecb8c1078385524dca5ceb45ce64
Lervin Urena <lurena@contractor.appsbees.com>
2022-12-28 13:44:41 -0400

    Feat: Add searchbar for changing the repo

--- components/commitList.js
+++ components/commitList.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import useSWR from 'swr'
 import CommitCard from './commitCard';
+import SearchBar from './searchBar';
 // import constants from '../utils/constants'
 
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function CommitList() { 
     const [page, setPage] = useState(1);
-    const { data, error, isLoading } = useSWR(`http://localhost:4000/commits?page=${page}&perPage=10`, fetcher);
+    const [author, setAuthor] = useState('LervinU');
+    const [repo, setRepo] = useState('commit-tracker');
+    const { data, error, isLoading } = useSWR(`http://localhost:4000/commits?page=${page}&perPage=10&author=${author}&repoName=${repo}`, fetcher);
 
     const handleNewerCommits = () => {
         if(page > 1) {
@@ -19,11 +22,21 @@ export default function CommitList() {
         setPage((currentPage) => currentPage + 1);
     }
 
-    if (error) return <div class="text-lg flex items-center justify-center h-screen">Failed to load</div>
-    if (isLoading) return <div class="text-lg flex items-center justify-center h-screen">Loading...</div>
+    const onSearch = (ref) => {
+        const repoUrl = ref.current.value;
+        const repoUrlArr = repoUrl && repoUrl.split('/');
+        setRepo(repoUrlArr[repoUrlArr.length - 1])
+        setAuthor(repoUrlArr[repoUrlArr.length - 2])
+    }
+
+    if (error) return <div className="text-lg flex items-center justify-center h-screen">Failed to load</div>
+    if (isLoading) return <div className="text-lg flex items-center justify-center h-screen">Loading...</div>
+    if (data.length === 0) return <div className="text-lg flex items-center justify-center h-screen">No more commits</div>
 
     return (
-        <div class="mt-8 mx-auto">
+        <div className="mt-8 mx-auto">
+            <SearchBar onSearch={onSearch}/>
+            <div>
             {data.map(commit => {
                 return (
                     <div>
@@ -31,11 +44,12 @@ export default function CommitList() {
                     </div>
                 )
             })}
-            <div class="flex items-center justify-center mb-8 mt-2">
-                <button onClick={handleNewerCommits} class="bg-gray-900 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-l">
+            </div>
+            <div className="flex items-center justify-center mb-8 mt-2">
+                <button onClick={handleNewerCommits} className="bg-gray-900 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-l">
                     Newer
                 </button>
-                <button onClick={handleOlderCommits} class="bg-gray-900 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-r">
+                <button onClick={handleOlderCommits} className="bg-gray-900 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-r">
                     Older
                 </button>
             </div>
